d2ba3d2b32bedc5e80efd39559c5d574dd94a7c1
marekvoel <33312600+marekvoel@users.noreply.github.com>
2017-11-17 11:01:35 +0100

    Version 1.4
    Full Working

--- mv_pwm.py
+++ mv_pwm.py
@@ -1,28 +1,81 @@
+#import
 import time
 import RPi.GPIO as GPIO
+
+#settings
+GPIO.cleanup
+GPIO.setwarnings(False)
+y = 26 #PWM-PIN
+
+#info
 print("------------------------------------------------")
 print("Software-PWM for stepper-motors by Marek Völckel")
+print("----> Version 1.4 (WORKING)")
+print("----> ")
 print("------------------------------------------------")
-'''variables'''
+
+
 print("-------")
 print("Config")
 print("-------")
-a = float(input("Frequenz eingeben:"))
-e = float(input("DutyCicle eingeben:"))
-y = 26
-'''frequency limit  '''
-if a > 1024:
+
+#frequency input and limiter
+a= float(input("Frequenz eingeben (1-1023):"))
+if a > 1023:
     print("-----------------------------------------")
     print("Die Frequenz darf nicht über 1023 liegen!")
     print("-----------------------------------------")
+    print("----> EXIT <----")
     exit()
+#limiter
+elif a < 1:
+    print("---------------------------------------")
+    print("Die Frequenz muss mindestens 1 betragen")
+    print("---------------------------------------")
+    print("----> EXIT <----")
+    exit()
+
+
+#DutyCile input and limiter
+e = float(input("DutyCicle eingeben (1-100):"))
+if e > 100:
+    print("-----------------------------------------")
+    print("Der DutyCicle darf nicht über 100% liegen!")
+    print("-----------------------------------------")
+    print("----> EXIT <----")
+    exit()
+#limiter
+elif a < 1:
+    print("------------------------------------------")
+    print("Der DutyCicle muss mindestens 1% betragen!")
+    print("------------------------------------------")
+    print("----> EXIT <----")
+    exit()
+
+
+#Rotating time input and limiter
+t = float(input("Drehzeit in Sekunden:"))
+if t > 60:
+    print("-----------------")
+    print("Drehzeit zu hoch!")
+    print("-----------------")
+    print("----> EXIT <----")
+    exit()
+#limiter 
+elif t < 2:
+    print("----------------------------------------")
+    print("Die Drehzeit muss mindestens 2 betragen!")
+    print("----------------------------------------")
+    print("----> EXIT <----")
+    exit()
+
+
+
+
+#Rotating process
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(y, GPIO.OUT) #PWM PIN
-pwm = GPIO.PWM (y,a)  # Frequenz: 2 Hertz
-pwm.start(e)           # Duty-Cycle: 50%
-time.sleep(10)
-
-#pwm.ChangeFrequency(950)    #Frequenz: 900
-#pwm.ChangeDutyCycle(50)           #DutyCycle: 50%
-
-GPIO.cleanup
+GPIO.setup(y, GPIO.OUT) 
+pwm = GPIO.PWM (y,a)  
+pwm.start(e)           
+time.sleep(t)
+pwm.stop(0)
