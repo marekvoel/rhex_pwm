e55cc7c4c2e7230bfb3d350ab9bfa6dfe0d7842c
marekvoel <33312600+marekvoel@users.noreply.github.com>
2017-11-17 09:21:50 +0100

    Working!
    First Working Alpha

--- softpwm.py
+++ softpwm.py
@@ -1,8 +1,14 @@
-import RPi.GPIO as gpio
-gpio.setup(26,gpio.OUT) #PWM PIN
-pwm = gpio.PWM (26, 2)  # Frequenz: 2 Hertz
+import sys
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(26, GPIO.OUT) #PWM PIN
+pwm = GPIO.PWM (26,900)  # Frequenz: 2 Hertz
 pwm.start(50)           # Duty-Cycle: 50%
 time.sleep(10)
 
-pwm.ChangeFrequency(800)    #Frequenz: 800
-pwm.DutyCycle(50)           #DutyCycle: 50%
+#pwm.ChangeFrequency(950)    #Frequenz: 900
+#pwm.ChangeDutyCycle(50)           #DutyCycle: 50%
+
+GPIO.cleanup
+
